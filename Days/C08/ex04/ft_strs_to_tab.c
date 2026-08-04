e9427b9fb2a153a8fbd3db64ff9e87a54da99a65
alimoutia <alimoutia38@gmail.com>
2026-08-05 00:01:13 +0100

    C08

--- Days/C08/ex04/ft_strs_to_tab.c
+++ Days/C08/ex04/ft_strs_to_tab.c
@@ -11,15 +11,42 @@ int hseb_tol(char *str)
 	return (i);
 }
 
-char *copy_lina(char *arr, )
+char *copy_lina(char *strr)
+{
+	int i;
+	char *ptr;
+
+	i = 0;
+	ptr = malloc(sizeof(char) * (hseb_tol(strr) + 1));
+	if (!ptr)
+		return (NULL);
+	while (strr[i])
+	{
+		ptr[i] = strr[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
 
 struct s_stock_str *ft_strs_to_tab(int ac, char **av)
 {
 	t_stock_str *arr;
+	int i;
 
 	if (ac < 0)
 		return (NULL);
 	arr = malloc(sizeof(t_stock_str) * (ac + 1));
 	if (!arr)
 		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		arr[i].size = hseb_tol(av[i]);
+		arr[i].str = av[i];
+		arr[i].copy = copy_lina(av[i]);
+		i++;
+	}
+	arr[i].str = NULL;
+	return (arr);
 }
---
+++ Days/C08/ex05/ft_show_tab.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		ft_putstr(par[i].str);
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstr(par[i].copy);
+		write(1, "\n", 1);
+		i++;
+	}
+}
