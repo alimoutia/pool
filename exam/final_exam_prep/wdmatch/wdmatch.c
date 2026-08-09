67f809ed3eda133edde1927d5ec6d5d0ab27ad85
alimoutia <alimoutia38@gmail.com>
2026-08-09 11:42:13 +0100

    updates

--- exam/final_exam_prep/wdmatch/wdmatch.c
+++ exam/final_exam_prep/wdmatch/wdmatch.c
@@ -1,43 +1,28 @@
 #include <unistd.h>
 
-int checkword(char **argv)
-{
-    int count = 0;
-    int i = 0;
-    int j = 0;
-    while (argv[1][i])
-    {
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
-        while (argv[2][i] != ' ' && )
-        {
-
-        }
-    }
-}
-
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
     int i = 0;
     int j = 0;
-    int word_printed = 0;
-    while (argv[1][i])
+
+    if (argc == 3)
     {
-        if (argv[1][i] == " " || argv[1][i] == "\t")
-            word_printed = 1;
-        while (argv[2][j])
+        while (argv[2][j]) // Advance j through the second string
         {
-            if (argv[1][i] == argv[1][j])
-                write(1, &argv[1][j], 1);
-            if (word_printed == 1)
-                write(1, " ", 1);
+            if (argv[1][i] == argv[2][j]) // If character matches, advance i in the first string
+                i++;
             j++;
         }
-        i++;
+        if (argv[1][i] == '\0') // If i reached the end of the first string, all characters were matched in order
+        {
+            i = 0;
+            while (argv[1][i])
+            {
+                write(1, &argv[1][i], 1);
+                i++;
+            }
+        }
     }
+    write(1, "\n", 1);
+    return (0);
 }
